047138a7cce8f188fa36e8584a163f031953decd
minaj27 <minaj13m@gmail.com>
2023-12-04 23:52:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+function handleHow(){
+    alert("I am fine,Thanks you!");
+    document.getElementById("bg").style.backgroundColor ="green";
+}
+
+function handleMorning(){
+    alert("Good Morning!!");
+    document.getElementById("bg").style.backgroundColor ="yellow";
+} 
+
+function handleAfternoon(){
+    alert("Good Afternoon!");
+    document.getElementById("bg").style.backgroundColor ="silver";
+}
+function handleEvening(){
+    alert("Good Evening!!");
+    document.getElementById("bg").style.backgroundColor ="coral";
+}
+function handleNight(){
+    alert("Good Night!!");
+    document.getElementById("bg").style.backgroundColor ="gray";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
